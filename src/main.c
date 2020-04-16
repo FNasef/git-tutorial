83f4b8d9e772d3d952d0b008a6512420e55cdd9d
Alex Hoffman <alxhoff@gmail.com>
2020-04-16 14:56:50 +0200

    Actualizing gitignore

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
+
 #include <stdio.h>
+#include <espl_lib.c>
+#include <espl_lib.h>
 int main() {
     int n;
     char answer, y;
     do {
     printf("please, enter a Number! \n");
     scanf("%d", &n);
-    char* num_to_words(int n);
+    printf("%s\n", num_to_words( n));
     printf("Do you want to use it again ? (y/n)\n");
     scanf("%s",&answer);
     }
